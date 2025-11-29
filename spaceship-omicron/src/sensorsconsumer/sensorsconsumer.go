23c1ae4b06f6e3ef19f1d44a0c4afbb62b19b96f
aamadaminov <aamadaminov@gmail.com>
2025-11-29 16:31:32 +0500

    producer from kafka to clickhouse

--- spaceship-omicron/src/sensorsconsumer/sensorsconsumer.go
+++ spaceship-omicron/src/sensorsconsumer/sensorsconsumer.go
@@ -1,79 +1,50 @@
 package main
 
 import (
-	// "crypto/tls"
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
-	// "github.com/ClickHouse/clickhouse-go/v2"
-	// "github.com/ClickHouse/clickhouse-go/v2/lib/driver"
 )
-
-// start clickhouse
-// func connect() (driver.Conn, error) {
-// 	var (
-// 		ctx       = context.Background()
-// 		conn, err = clickhouse.Open(&clickhouse.Options{
-// 			Addr: []string{"localhost:9009"},
-// 			Auth: clickhouse.Auth{
-// 				Database: "default",
-// 				Username: "default",
-// 				Password: "",
-// 			},
-// 			ClientInfo: clickhouse.ClientInfo{
-// 				Products: []struct {
-// 					Name    string
-// 					Version string
-// 				}{
-// 					{Name: "an-example-go-client", Version: "0.1"},
-// 				},
-// 			},
-// 			Debugf: func(format string, v ...interface{}) {
-// 				fmt.Printf(format, v)
-// 			},
-// 			TLS: &tls.Config{
-// 				InsecureSkipVerify: true,
-// 			},
-// 		})
-// 	)
-
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	if err := conn.Ping(ctx); err != nil {
-// 		if exception, ok := err.(*clickhouse.Exception); ok {
-// 			fmt.Printf("Exception [%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
-// 		}
-// 		return nil, err
-// 	}
-// 	return conn, nil
-// }
-
-// end Clickhouse
 
 func main() {
 	// Clickhouse start
-	// conn, err := connect()
-	// if err != nil {
-	// 	panic(err)
-	// }
+	conn, err := clickhouse.Open(&clickhouse.Options{
+		Addr: []string{"localhost:9000"},
+		Auth: clickhouse.Auth{
+			Database: "spaceshipomicron_db",
+			Username: "username",
+			Password: "password",
+		},
+		// TLS: &tls.Config{},
+	})
+	if err != nil {
+		panic(err)
+	}
 
-	// ctx := context.Background()
-	// rows, err := conn.Query(ctx, "SELECT name, toString(uuid) as uuid_str FROM system.tables LIMIT 5")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// clickhouse version show begin
+	v, err := conn.ServerVersion()
+	fmt.Println(v)
+	if err != nil {
+		panic(err)
+	}
+	// Clickhouse version show end
 
-	// for rows.Next() {
-	// 	var name, uuid string
-	// 	if err := rows.Scan(&name, &uuid); err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	log.Printf("name: %s, uuid: %s", name, uuid)
-	// }
-	// Clickhouse end
+	// clickhouse check db exist begin
+	err2 := conn.Exec(context.Background(), `
+    CREATE TABLE IF NOT EXISTS coords1
+(
+    s1 String,
+    s2 String,
+) ENGINE = MergeTree()
+ PRIMARY KEY s1
+`)
+	if err2 != nil {
+		panic(err2)
+	}
+	// clickhouse check db exist end
 
 	// kafka start
 	config := &kafka.ConfigMap{
@@ -97,7 +68,16 @@ func main() {
 		ev := consumer.Poll(1000)
 		switch e := ev.(type) {
 		case *kafka.Message:
-			fmt.Println(string(e.Value))
+			//fmt.Println(string(e.Value))
+			str1 := fmt.Sprint(string(e.Value))
+			fmt.Println(str1)
+			// insert to clickhouse
+			//str2 := "abc"
+			err3 := conn.Exec(context.Background(), fmt.Sprintf("INSERT INTO spaceshipomicron_db.coords1 (s1) VALUES ('%s')", str1))
+			if err3 != nil {
+				panic(err3)
+			}
+			//			insert to clickhouse end
 		case kafka.Error:
 			fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
 			run = false
@@ -106,6 +86,9 @@ func main() {
 
 		}
 
-	}
+	} // env, err := GetNativeTestEnvironment()
+	// if err != nil {
+	// 	return err
+	// }
 	consumer.Close()
 }
